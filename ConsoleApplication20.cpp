38929f360fead48617b931b1a85e917f218b5c78
saleh muhammad <74898333+salehmalik121@users.noreply.github.com>
2021-09-14 17:57:44 +0500

    Update ConsoleApplication20.cpp

--- ConsoleApplication20.cpp
+++ ConsoleApplication20.cpp
@@ -1,20 +1,33 @@
-// ConsoleApplication20.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <cstring>
+#include <vector>
+using namespace std;
+void print(vector<char> input)
+{
+    for (int i = 0; i < input.size(); i++) {
+        if (input.at(i) >= 97 && input.at(i) <= 122) {
+            input.at(i) = input.at(i) - 32;
+        }
+        cout << input.at(i);
+    }
+}
+
+void StringToArray() {
+    string Data;
+
+    cout << "Enter word : ";
+    cin >> Data;
+    vector<char>v(Data.begin(),Data.end());
+    print(v);
+    
+    
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+     
+    
+    StringToArray();
+    
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
