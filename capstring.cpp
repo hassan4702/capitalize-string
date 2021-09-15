7e9b95bf9ebe9bc1d01d02f859f51c5739f9eccc
TS <80575500+tssoftwares4702@users.noreply.github.com>
2021-09-15 13:03:29 +0500

    Create capstring.cpp

---
+++ capstring.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+using namespace std;
+void print(vector<char> input)
+{
+    for (int i = 0; i < input.size(); i++) {
+        if (input.at(i) >= 97 && input.at(i) <= 122) {
+            input.at(i) = input.at(i) - 32;
+        }
+        cout << input.at(i);
+    }
+}
+
+void StringToArray() {
+    string Data;
+
+    cout << "Enter word : ";
+    cin >> Data;
+    vector<char>v(Data.begin(),Data.end());
+    print(v);
+
+
+}
+
+int main()
+{
+
+
+    StringToArray();
+
+
+}
